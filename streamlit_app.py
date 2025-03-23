1a947302036a5e999a8859f35019b3b46819b3c5
meetshannow <meetshannow@gmail.com>
2025-03-23 18:32:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredients_list :
         st.subheader(fruit_chosen + ' Nutrition Information')
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
         #sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + str(search_on))
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         #st.write(ingredients_string)
 
